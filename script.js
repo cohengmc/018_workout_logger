5ba9062422e63cdbe18cde70a90923dd047e3979
Geoffrey <you@example.comcohengmc@gmail.com>
2024-04-06 08:51:42 -0400

    2 workouts

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const draggableElement = document.querySelector('p[draggable="true"]');
-const dropElement = document.getElementById("drop-target");
+const draggableElements = document.querySelectorAll('p[draggable="true"]');
+const dropElements = document.querySelectorAll(".drop-target");
 
+draggableElements.forEach(draggableElement => {
+    draggableElement.addEventListener("dragstart", (event) =>
+        event.dataTransfer.setData("text", event.target.id),
+    );
 
-draggableElement.addEventListener("dragstart", (event) =>
-event.dataTransfer.setData("text", event.target.id),
-);
-
-dropElement.addEventListener("dragenter", (event) => {
-  event.preventDefault();
 });
 
-dropElement.addEventListener("dragover", (event) => {
-  event.preventDefault();
-});
+dropElements.forEach(dropElement => {
+    dropElement.addEventListener("dragenter", (event) => {
+        event.preventDefault();
+    });
 
-dropElement.addEventListener("ondragover", (event) => {
-    event.preventDefault();
-  });
+    dropElement.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+
+    dropElement.addEventListener("ondragover", (event) => {
+        event.preventDefault();
+    });
+});
 
 function onDrop(event) {
     const data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
     event.preventDefault();
-  }
+}
